1dfd7ec15be554e62a28681f35b680fcff16bb5f
mveillon <49701743+mveillon@users.noreply.github.com>
2022-07-31 20:12:26 -0500

    better pitch test

--- tests/nextPitch.test.ts
+++ tests/nextPitch.test.ts
@@ -1,18 +1,19 @@
 import { nextPitch } from "../src/ml/nextPitch";
-import { pitchToInd } from "../src/ml/mappings";
 import { state, resetState } from "../src/baseballLogic/GameState";
-import { changePitcher } from "../src/ui/scorebug";
+import { allPitchers } from "../src/baseballLogic/Pitcher";
 
 test('next pitch', () => {
     resetState();
     state.lineup = ['R'];
-    changePitcher('Justin Verlander');
+    const pitcher = allPitchers['Justin Verlander'];
+    state.pitcher = pitcher;
     for (let b = 0; b < 4; b++) {
         for (let s = 0; s < 3; s++) {
             for (let p = 0; p < 2; p++) {
                 state.balls = b;
                 state.strikes = s;
-                expect(nextPitch() in pitchToInd).toBe(true);
+                state.lineup[0] = p ? 'R' : 'L';
+                expect(nextPitch() in pitcher.pitches).toBe(true);
             }
         }
     }
